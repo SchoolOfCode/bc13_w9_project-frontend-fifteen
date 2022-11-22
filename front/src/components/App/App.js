bbbf37e75208b8edb1d09e78526bd9602cce04f4
Julio Camargo <jcamargo@hotmail.co.uk>
2022-11-22 16:05:11 +0000

    some scss done

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,18 +1,20 @@
-import './App.css';
+
 // import ReturnDataField from './components/ReturnDataField';
 import Input from '../Input/Input';
 import ResultsData from '../ResultsData/ResultsData';
-
+import Header from '../Header';
 
 
 
 function App() {
   return (
-   <>
+    <div id='body'>
+   <div className='main'>
+      <Header />
      <Input />
-     <ResultsData />
-       
-   </>
+     <ResultsData />  
+   </div>
+   </div>
   );
 }
 
---
+++ front/src/components/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+  return(
+    <div className='header'>
+      <h1>Im a Header</h1>
+    </div>
+  )
+}
+export default Header;
