01b9c30c7b9c64f971114be87894ec0e0a2f91f1
Rachel Morris <rachelmorris262@outlook.com>
2022-11-23 15:47:29 +0000

    add an object to an array

--- front/src/components/Input/Input.js
+++ front/src/components/Input/Input.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
 
-import { useState } from "react"
+let nextId = 0;
+
+export default function Input() {
+  const [title, setTitle] = useState("");
+  const [array, setArray] = useState([]);
+  
+  function handleChange (event) {
+    setTitle(event.target.value)
+  }
+
+  function handleClick () {
+    setTitle("");
+    setArray([...array, { id: nextId++, title: title }]);
+    console.log(array)
+  }
+
+  return (
+    <>
+      <h1>Test Page</h1>
+      <input value={title} onChange={handleChange} />
+      <button onClick={handleClick} > Add </button>
+      <ul>
+        {array.map((array) => (
+          <li key={array.id}>{array.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+
+
+/*
+import { useRef } from "react"
 import React from 'react'
 
 
@@ -26,20 +60,38 @@ const randomData = [
 ]
 
 export default function Input() {
-  const [file, setFile] = useState("")
+  const inputTitle = useRef(null);
+  const inputDescr = useRef(null);
+  const inputSnip = useRef(null);
 
+  
+ 
+  function handleClick () {
+    let newTitle = inputTitle.current.value;
+    let newDescr = inputDescr.current.value;
+    let newSnip = inputSnip.current.value;
+    let newID = randomData.length +1
+    
+    let newObject = {id: newID, title:newTitle, comment:newDescr, code:newSnip }
+    console.log(newObject)
+   
+    
+    // When button is pressed ✅ 
+    // Take in data from the inputs and create a new object ✅
+    // Then push the new object to randomData
 
-
+  }
 
 
   
 return (
-<>
-<input id= "input" type="text" placeholder="Add title"></input>
-<input id= "input" type="text" placeholder="Add description"></input>
-<input id= "input" type="text" placeholder="Add code snippet"></input>
-<button id="button" >Add</button>
-</>
+  <>
+    <input id= "input" ref={inputTitle} type="text" placeholder="Add title"></input>
+    <input id= "input" ref={inputDescr}type="text"  placeholder="Add description"></input>
+    <input id= "input" ref={inputSnip} type="text"  placeholder="Add code snippet"></input>
+    <button id="button" onClick={handleClick}>Add</button>
+  </>
 )
 }
 
+*/
